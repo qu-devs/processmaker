6dc43905d5cd39645a91ba617128e42968ecb367
Sarah <salhumud@qu.edu.sa>
2024-08-11 09:03:57 +0300

    Add resources of the packages

---
+++ resources/lang/vendor/reportMaker/en/report.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'Reports' => 'Reports',
+    'Report' => 'Report',
+    'Show' => 'Show',
+    'Close' => 'Close',
+    'Save' => 'Save',
+    'Name' => 'Name',
+    'Status' => 'Status',
+    'Participants' => 'Participants',
+    'Started' => 'Started',
+    'Completed' => 'Completed',
+    'Add Report' => 'Add Report',
+    "Add Custom Column" => "Add Custom Column",
+    "Available Columns" => "Available Columns",
+    "Active Columns" => "Active Columns",
+    "Reset to Default" => "Reset to Default",
+    "Finding available columns..." => "Finding available columns...",
+    "Select option" => "Select option",
+    "Drag any columns you want to view in your table from right to left. You may also sort, configure, and remove columns." => "Drag any columns you want to view in your table from right to left. You may also sort, configure, and remove columns."
+];
